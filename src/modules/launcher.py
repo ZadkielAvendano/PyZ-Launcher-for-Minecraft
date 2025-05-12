47117d9e8cac4e24926dfdc54a8486cf0a6f1c7b
Zadkiel Avendano <zadkiel4600@gmail.com>
2025-05-12 18:29:05 -0400

    Snapshot Support

--- src/modules/launcher.py
+++ src/modules/launcher.py
@@ -70,11 +70,11 @@ def launch_game(page: ft.Page, version_id: str, status_text_control: ft.Text, bu
 
 current_max = 0
 
-def set_progress(progress: int, progress_bar: ft.ProgressBar):
+def set_progress(page: ft.Page, progress: int, progress_bar: ft.ProgressBar, progress_text: ft.Text):
     if current_max != 0:
         progress_bar.value = progress / current_max
         progress_bar.update()
-        print(f"{progress}/{current_max}")
+        update_status_safe(page, progress_text, f"{progress}/{current_max}")
 
 def set_max(new_max: int):
     global current_max
@@ -82,14 +82,15 @@ def set_max(new_max: int):
 
 
 
-def install_version(page: ft.Page, version_id: str, buttons_to_disable: list, progress_window: ft.AlertDialog, progress_bar: ft.ProgressBar, status_text: ft.Text, refresh):
+def install_version(page: ft.Page, version_id: str, buttons_to_disable: list, progress_window: ft.AlertDialog,
+                    progress_bar: ft.ProgressBar, status_text: ft.Text, progress_text: ft.Text, refresh):
     try:
         set_controls_enabled_safe(page, buttons_to_disable, False)
         update_status_safe(page, status_text, f"Checking version: {version_id}...")
 
         callback={
             "setStatus": lambda status: update_status_safe(page, status_text, f"{status}"),
-            "setProgress": lambda progress: set_progress(progress, progress_bar),
+            "setProgress": lambda progress: set_progress(page, progress, progress_bar, progress_text),
             "setMax": set_max
             }
 
@@ -107,10 +108,14 @@ def install_version(page: ft.Page, version_id: str, buttons_to_disable: list, pr
 
 
 def get_versions() -> dict:
+    """Returns a dict of all Minecraft versions (release, snapshot)"""
+
     versions = mll.utils.get_version_list()
     installed = mll.utils.get_installed_versions(app_settings.return_mc_directory())
     return {
-        "installed": [v["id"] for v in installed],
+        "installed": installed,
         "releases": [v["id"] for v in versions if v["type"] == "release"],
-        "snapshots": [v["id"] for v in versions if v["type"] == "snapshot"]
+        "snapshots": [v["id"] for v in versions if v["type"] == "snapshot"],
+        "old_beta": [v["id"] for v in versions if v["type"] == "old_beta"],
+        "old_alpha": [v["id"] for v in versions if v["type"] == "old_alpha"]
     }
